6afdcbff2eecd70c0ac7ee85cebe7e96d26f6410
chinmayee2002 <c1chinmayee.sahoo@gmail.com>
2025-11-02 20:27:24 +0530

    css-chapter-1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,19 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div style={{
+      display:'flex',
+      justifyContent:'center',
+      alignItems:'center',
+      height:'97vh',
+      backgroundColor:'#313131'
+    }}>
+      <div>
+
+      <h1>Hello, World!</h1>
+      <p>Welcome to my React application.</p>
+      </div>
+    </div>
   )
 }
 
